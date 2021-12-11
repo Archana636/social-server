bfe58607316a0a5d0ee3e7e3fa42f363f48d9401
Senthil S <senthil.s@kyndryl.com>
2021-12-11 19:05:13 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://login:mtv3zcDyHiXkWfxu@login.nykrw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
